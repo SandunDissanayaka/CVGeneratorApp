73f7a895c6b55e098e73feaa73f0db72f4293798
SandunDissanayaka <ar117119@fhss.sjp.ac.lk>
2026-04-27 23:42:14 +0530

    Home page created

--- CVGeneratorApp/HomePage.xaml.cs
+++ CVGeneratorApp/HomePage.xaml.cs
@@ -1,26 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CVGeneratorApp
 {
-    /// <summary>
-    /// Interaction logic for HomePage.xaml
-    /// </summary>
     public partial class HomePage : Page
     {
         public HomePage()
         {
             InitializeComponent();
+
+            // Note: In the future, we will write a function here to load 
+            // real data from the SQL database into the 'pnlCVList' WrapPanel.
         }
     }
 }
